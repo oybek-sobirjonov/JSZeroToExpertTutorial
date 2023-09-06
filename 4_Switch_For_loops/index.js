3ab8be78c8a283d371590e1011860e526ac7ca86
Oybek Sobirjonov <87658070+oybek-sobirjonov@users.noreply.github.com>
2023-09-06 19:23:13 +0500

    switch for loops

--- 4_Switch_For_loops/index.js
+++ 4_Switch_For_loops/index.js
@@ -1,18 +1,38 @@
-// if -> <, >, <=, >=, ==, ==
-// switch -> ===
+// if va switch ning asosiy farqi quyidagicha:
+// if qabul qiladigan holatlar <, >, <=, >=, ==, ==
+// switch qabul qiladigan holat ===
 
-let data = "qor";
+// let data = "qor";
 
-switch (data) {
-    case "bulut":
-        console.log("Havo bulut");
-        break;
-    case "qor":
-        console.log("Havo qor");
-        break;
-    case "yomgir":
-        console.log("Havo yomg'ir");
-        break;
-    default:
-        console.log("Havo nomalum");
+// switch (data) {
+//     case "bulut":
+//         console.log("Havo bulut");
+//         break;
+//     case "qor":
+//         console.log("Havo qor");
+//         break;
+//     case "yomgir":
+//         console.log("Havo yomg'ir");
+//         break;
+//     default:
+//         console.log("Havo nomalum");
+// }
+
+// console.log("Muhammad");
+// console.log("Muhammad");
+// console.log("Muhammad");
+// console.log("Muhammad");
+// console.log("Muhammad");
+// console.log("Muhammad");
+// console.log("Muhammad");
+// console.log("Muhammad");
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+// for loop - looks like a stairs
+// for (beginnig point, target (until where we are going), steps (how many steps each time))
+for (i = 0; i < 5; i++) {
+    console.log(i);
 }
