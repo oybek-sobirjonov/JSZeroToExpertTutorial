4a5628023612470254f9d029f1ebdef3a395adda
Oybek Sobirjonov <87658070+oybek-sobirjonov@users.noreply.github.com>
2023-09-06 18:50:36 +0500

    Create index.js
    switch for loops

---
+++ 4_Switch_For_loops/index.js
@@ -0,0 +1,18 @@
+// if -> <, >, <=, >=, ==, ==
+// switch -> ===
+
+let data = "qor";
+
+switch (data) {
+    case "bulut":
+        console.log("Havo bulut");
+        break;
+    case "qor":
+        console.log("Havo qor");
+        break;
+    case "yomgir":
+        console.log("Havo yomg'ir");
+        break;
+    default:
+        console.log("Havo nomalum");
+}
